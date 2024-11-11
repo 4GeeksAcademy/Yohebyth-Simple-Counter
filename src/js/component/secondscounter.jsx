721264d89c9eea6d99e09371cd51496fd5a90d67
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2024-11-11 19:19:53 +0000

    border

--- src/js/component/secondscounter.jsx
+++ src/js/component/secondscounter.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SecondsCounter = ({time}) => {
     return (
         <div className="col" style={{height: "5%"}}>
-            <div className="p-2 text-white fs-1 fw-bolder">
+            <div className="num p-2 m-2 text-white fs-1 fw-bolder">
                 {time}
             </div>
         </div>       
