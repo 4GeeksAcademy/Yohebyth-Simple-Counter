dc87bdac90be48a9e1c1c7df7e5c67a4dcc46afa
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2024-11-13 22:55:59 +0000

    buttons of paused, start, reset

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,18 +5,23 @@ import SecondsCounter from "./secondscounter.jsx";
 //create your first component
 const Home = () => {
 	const [count, setCount] = useState(0)
+	const [run, setRun] = useState(true)
 
 	useEffect(() => {
-		setTimeout(() => {
-			setCount(value => value + 1)
-		}, 1000)
-	},[count])
+		if(run){
+			setTimeout(() => {
+				setCount(value => value + 1)
+			}, 1000)
+		}
+	},[count, run])
 
 	return (
 		<div className="text-center">
 			<div className="container text-center bg-dark">
-				<div className="row row-cols-lg-7 g-2 g-lg-3">
+				<div className="row row-cols-1 g-2 g-sm-3">
 					<SecondsCounter time={<span className="fa-regular fa-clock"></span>} />
+				</div>				
+				<div className="row row-cols-1 row-cols-sm-6 g-2 g-sm-3">					
 					<SecondsCounter time={Math.floor(count / 100000) % 10} />
 					<SecondsCounter time={Math.floor(count / 10000) % 10} />
 					<SecondsCounter time={Math.floor(count / 1000) % 10} />
@@ -24,8 +29,28 @@ const Home = () => {
 					<SecondsCounter time={Math.floor(count / 10) % 10} />
 					<SecondsCounter time={Math.floor(count % 10)} />
 				</div>
-			</div>
-			
+			</div>	
+			<button type="button" 
+				className="btn btn-dark"
+				onClick={ () => setRun(false)}
+			>
+				<i className="fa-solid fa-pause"></i>								
+			</button>		
+			<button type="button" 
+				className="btn btn-dark"
+				onClick={ () => setRun(true)}
+			>
+				<i className="fa-solid fa-play"></i>
+			</button>
+			<button type="button" 
+				className="btn btn-dark"
+				onClick={ () =>{
+					setCount(0) 
+					setRun(true)
+				}}
+			>
+				<i className="fa-solid fa-rotate"></i>
+			</button>
 		</div>
 	);
 };
